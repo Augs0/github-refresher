ad26f402d15ac0cd5fa1f168bb05631667041483
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-10-12 13:20:34 +0100

    more content added to the main tag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>GitHub practice</h1>
+        <h1>GitHub practice -2</h1>
       </header>
       <main>
         <h2>Add your contribution here:Thank you August from Taslima</h2>
